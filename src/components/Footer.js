8eb853841abde0f898da0cc057df03480d09f4b3
LyTing <dce957351@gmail.com>
2022-02-26 23:24:22 +0800

    first commit (Routing in React Start)

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div className="footer">LyTing 2021 ©</div>;
+};
+
+export default Footer;
---
+++ src/pages/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Search from "../components/Search";
+
+const Homepage = () => {
+  const auth = "563492ad6f9170000100000195929b099f914577a36294801aa1dc18";
+  return (
+    <div>
+      <h1>This is homepage.</h1>
+    </div>
+  );
+};
+
+export default Homepage;

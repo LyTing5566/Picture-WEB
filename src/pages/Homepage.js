8e192910f722405ce5008ac4ec75cfb051e7fb34
LyTing <dce957351@gmail.com>
2022-02-27 02:12:10 +0800

    Routes && Styles Done

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,8 +4,8 @@ import Search from "../components/Search";
 const Homepage = () => {
   const auth = "563492ad6f9170000100000195929b099f914577a36294801aa1dc18";
   return (
-    <div>
-      <h1>This is homepage.</h1>
+    <div style={{ minHeight: "100vh" }}>
+      <Search />
     </div>
   );
 };
